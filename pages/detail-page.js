181657151ee86c0a7aa0bba45a14f1cfd5a35115
mbosselut <duchmol7@hotmail.com>
2019-10-09 14:30:55 +0200

    Set up form validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,23 +1,46 @@
 function submitComment() {
-    // assign DOM elements
+    // assigning DOM elements
     const inputField = document.getElementById('name');
-    const name = inputField.value;
+    let name = inputField.value;
     const textArea = document.getElementById('msg');
     const msg = textArea.value;
-    // create new elements
+    // comment validation
+    if (doesNotPassAllValidations(name, msg)) {
+        return null
+    }
+    if (name[0] !== name[0].toUpperCase()) {
+        name = name[0].toUpperCase() + name.substring(1);
+    }
+    // creating new elements
     const comment = document.createElement('section');
     const h3 = document.createElement('h3');
     const p = document.createElement('p');
-    // add content to new elements
+    // adding content to new elements
     h3.innerHTML = `${name} said :`;
     p.innerHTML = msg;
     comment.classList.add('comment');
     comment.appendChild(h3);
     comment.appendChild(p);
-    // Add new comment to page
+    // Adding new comment to page
     const commentSection = document.getElementById('comments');
     commentSection.appendChild(comment);
 
     inputField.value = null;
     textArea. value = null;
+}
+
+function doesNotPassAllValidations(name, msg) {
+    if (!msg) {
+        alert('You forgot to fill in your message !');
+        return true;
+    }
+    if (!name) {
+        alert('You forgot to fill in your name !');
+        return true;
+    }
+    if (msg.length > 280) {
+        alert('Please write a shorter message !');
+        return true;
+    }
+    return false;
 }
