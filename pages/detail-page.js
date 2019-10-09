0575e8becff8b90fccf1547d012e8052a9546983
mbosselut <duchmol7@hotmail.com>
2019-10-09 13:53:08 +0200

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,18 @@
 function submitComment() {
-    console.log("Hello!");
+    // assign DOM elements
+    const inputField = document.getElementById('name');
+    const name = inputField.value;
+    const textArea = document.getElementById('msg');
+    const msg = textArea.value;
+    // create new elements
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+    // add content to new elements
+    h3.innerHTML = `${name} said :`;
+    p.innerHTML = msg;
+    comment.classList.add('comment');
+    comment.appendChild(h3);
+    comment.appendChild(p);
+    console.log(comment);
 }
